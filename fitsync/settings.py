95ab8baaee4736f51c8d71ebfc7b98ae5185b219
Adithyan K <adithyankeloth2244@gmail.com>
2026-03-10 17:12:10 +0530

    Config: Updated default sender email to fitsync007@gmail.com for system emails

--- fitsync/settings.py
+++ fitsync/settings.py
@@ -183,6 +183,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'adithyankeloth555@gmail.com'  # REPLACE WITH YOUR GMAIL
+EMAIL_HOST_USER = 'fitsync007@gmail.com'  # REPLACE WITH YOUR GMAIL
 EMAIL_HOST_PASSWORD = 'tslj lkpk zrpw briu'  # REPLACE WITH YOUR GMAIL APP PASSWORD
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
